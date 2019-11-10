84b8692aa0d72bd48023d3c72a13dce2be796686
rajaws2015 <rajaws2015@gmail.com>
2019-11-10 22:31:06 +0100

    Update find_free_memory.sh

--- find_free_memory.sh
+++ find_free_memory.sh
@@ -1,1 +1,8 @@
-free -mt | grep Total:| awk '{print $4}'
+#! /bin/bash
+TO="rajaws2015@gmail.com"
+ram_free=$(free -mt| grep Total:| awk '{print $4}')
+if [ $ram_free -le 5000 ]
+   then
+        echo "sending email because your RAM size is lower than 5000"
+        echo "subject:warning. RAM size is low"|sendmail $TO
+fi
